0e710a9499ab0802928af80faa025c6011958b23
sejatamboli <sejaltamboli0602@gmail.com>
2022-03-28 21:25:10 -0700

    MODIFIED: Add Edit and Delete Contact Function is Used in Address Book

--- src/com/infogalaxy/addressbookmanager/AdressBook.java
+++ src/com/infogalaxy/addressbookmanager/AdressBook.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class AdressBook {
     ArrayList<Contact> contactlist = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
-    Contact contact = new Contact();
 
     public void addContact() {
+        Contact contact = new Contact();
         System.out.println("Enter the First Name: ");
         contact.setFirstName(sc.next());
         System.out.println("Enter the Last Name: ");
@@ -27,12 +27,66 @@ public class AdressBook {
         contact.setZip(sc.next());
         contactlist.add(contact);
     }
-
     public void displayContact() {
         for (int i = 0; i < contactlist.size(); i++) {
             Contact contact = contactlist.get(i);
             System.out.println(contact.toString());
         }
+    }
+    public void editcontact() {
+        System.out.println("Enter the First Name: ");
+        String fname = sc.next();
+        int flag = 0;
+        for (int i = 0; i < contactlist.size(); i++) {
+            Contact contact = contactlist.get(i);
+            if (fname.equals(contact.getFirstName())) {
+                flag = 1;
+                break;
+           }
+        }
+        if (flag == 0) {
+            System.out.println("Contact Not found. ");
+        } else {
+            System.out.println("Contact is found. ");
+            Contact contact = new Contact();
+            System.out.println("Enter the Last Name: ");
+            contact.setLastName(sc.next());
+            System.out.println("Enter the Address: ");
+            contact.setAddress(sc.next());
+            System.out.println("Enter the City: ");
+            contact.setCity(sc.next());
+            System.out.println("Enter the State: ");
+            contact.setState(sc.next());
+            System.out.println("Enter the Mobile no: ");
+            contact.setMobno(sc.next());
+            System.out.println("Enter the Email: ");
+            contact.setEmail(sc.next());
+            System.out.println("Enter the Zip Code: ");
+            contact.setZip(sc.next());
+        }
+    }
+    public void findstate() {
+        System.out.println("Enter the State Name You Want to Search: ");
+        String stateName = sc.next();
+        for (int i = 0; i < contactlist.size(); i++) {
+            Contact contact = contactlist.get(i);
+            if (stateName.equals(contact.getState())) {
+                System.out.println(contact.getFirstName());
+                System.out.println(contact.getMobno());
+
+            }
+        }
+    }
+    public void deletecontact() {
+        System.out.println("Enter the First Name: ");
+        String delete = sc.next();
+        for(int i = 0; i < contactlist.size(); i++) {
+            Contact contact = contactlist.get(i);
+            if(delete.equals(contact.getFirstName())) {
+                contactlist.remove(i);
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -41,7 +95,7 @@ public class AdressBook {
         int choice;
         do {
             System.out.println("***** CONTACT INVENTORY MANAGEMENT *****");
-            System.out.println("\n1. ADD CONTACT \n2. DISPLAY CONTACT \n3. EXIT ");
+            System.out.println("\n1. ADD CONTACT \n2. DISPLAY CONTACT \n3. EDIT CONTACT \n4. FIND CONTACT \n5 DELETE CONTACT \n6 EXIT. ");
             System.out.println("Enter your Choice : ");
             choice = sc.nextInt();
             switch (choice) {
@@ -51,8 +105,17 @@ public class AdressBook {
                 case 2:
                     adressBook.displayContact();
                     break;
+                case 3:
+                    adressBook.editcontact();
+                    break;
+                case 4:
+                    adressBook.findstate();
+                    break;
+                case 5:
+                    adressBook.deletecontact();
+                    break;
             }
-        } while (choice != 3);
+        } while (choice != 6);
     }
 }
 
